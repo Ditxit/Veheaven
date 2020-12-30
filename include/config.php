96ab72843ae09d4ac66b5f9e81fd6add8ce60da2
Ditxit <gau.manish77@gmail.com>
2020-12-30 18:49:43 +0545

    Tested website in mobile phone using laptop IP

--- include/config.php
+++ include/config.php
@@ -3,6 +3,7 @@
     // do not include trailing slash '/' in the links
 
     define("SERVER_NAME","http://localhost/Veheaven");
+    //define("SERVER_NAME","http://192.168.1.71/Veheaven");
     define("API_ENDPOINT", SERVER_NAME.'/api');
 
     
