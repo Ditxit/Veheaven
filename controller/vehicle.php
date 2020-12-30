d1a63b25a4d0273f5fdc02c7691f0058aaa4ed4f
Ditxit <gau.manish77@gmail.com>
2020-12-30 18:43:34 +0545

    Uploading more datas; used_vehicle info & colors

--- controller/vehicle.php
+++ controller/vehicle.php
@@ -1,22 +1,27 @@
 <?php
     if(isset($_POST['vehicle-add'])){
+
         // Including global constants
         include_once '../include/config.php';
 
-        echo "<pre>";
-            var_dump($_POST);
-            var_dump($_FILES);
-            exit;
-        echo "</pre>";
+        // echo "<pre>";
+        //     var_dump($_POST);
+        //     var_dump($_FILES);
+        // echo "</pre>";
+        // exit;
 
-        // Handling Image Uploads First 
+        /* 
+        *   Handling image saving first
+        */
         $imageIdArray = [];
 
         // Opening curl connection
-        $cURLConnection = curl_init('http://localhost/Veheaven/api/image/save');
+        $cURLConnection = curl_init(API_ENDPOINT.'/image/save');
 
+        // Loopin each image file
         for($i = 0; $i < count($_FILES["vehicle-image"]["tmp_name"]); $i++){
 
+            // Creating new curl file object for each file
             $file = array(
                 'vehicle-image' => new CurlFile(
                     $_FILES["vehicle-image"]["tmp_name"][$i],
@@ -39,23 +44,109 @@
         curl_close($cURLConnection);
 
 
-        // Adding vehicle table data
+        /* 
+        *   Adding vehicle data in vehicle table
+        */
+        $data = [];
+        $data['token'] = $_POST['token'];
+        $data['vehicle-name'] = $_POST['vehicle-name'];
+        $data['vehicle-price'] = $_POST['vehicle-price'];
+        $data['vehicle-mileage'] = $_POST['vehicle-mileage'];
+        $data['vehicle-cc'] = $_POST['vehicle-cc'];
+        $data['vehicle-bhp'] = $_POST['vehicle-bhp'];
+        $data['vehicle-turn-radius'] = $_POST['vehicle-turn-radius'];
+        $data['vehicle-seat'] = $_POST['vehicle-seat'];
+        $data['vehicle-top-speed'] = $_POST['vehicle-top-speed'];
+        $data['vehicle-condition'] = isset($_POST['vehicle-condition']) ? $_POST['vehicle-condition'] : 2;
+        $data['vehicle-type'] = $_POST['vehicle-type'];
+        $data['vehicle-body'] = $_POST['vehicle-body'];
+        $data['vehicle-transmission'] = $_POST['vehicle-transmission'];
+        $data['vehicle-front-tyre'] = $_POST['vehicle-front-tyre'];
+        $data['vehicle-rear-tyre'] = $_POST['vehicle-rear-tyre'];
+        $data['vehicle-fuel'] = $_POST['vehicle-fuel'];
+        $data['vehicle-fuel-capacity'] = $_POST['vehicle-fuel-capacity'];
+        $data['vehicle-front-break'] = $_POST['vehicle-front-break'];
+        $data['vehicle-rear-break'] = $_POST['vehicle-rear-break'];
+        $data['vehicle-front-suspension'] = $_POST['vehicle-front-suspension'];
+        $data['vehicle-rear-suspension'] = $_POST['vehicle-rear-suspension'];
+        $data['vehicle-model'] = $_POST['vehicle-model'];
+
         $cURLConnection = curl_init(API_ENDPOINT.'/vehicle/add');
-        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $_POST);
+        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $data);
         curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
         
         $apiResponse = curl_exec($cURLConnection);
         curl_close($cURLConnection);
         $apiResponse = json_decode($apiResponse,TRUE);
 
-        if(isset($apiResponse) && isset($apiResponse['inserted_id'])){
-            $vehicleId = $apiResponse['inserted_id'];
-            setcookie('toast_message', "Vehicle added successfully", time()+60*60, "/");
-            header('Location: ../profile');
-            exit;
-        }else{
+        // if error inserting in vehicle table redirect with error message
+        if(!isset($apiResponse) || !isset($apiResponse['inserted_id'])){
             setcookie('toast_message', "Error adding vehicle", time()+60*60, "/");
             header('Location: ../explore');
             exit;
         }
+
+        $vehicleId = $apiResponse['inserted_id'];
+
+        /*
+        *   Adding data in vehicle_image table  
+        */
+        $data = [];
+        $data['token'] = $_POST['token'];
+        $data['vehicle-id'] = $vehicleId;
+        $data['image-id'] = json_encode($imageIdArray);
+
+        $cURLConnection = curl_init(API_ENDPOINT.'/vehicle/image/add');
+        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        
+        $apiResponse = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+        $apiResponse = json_decode($apiResponse,TRUE);
+
+        /*
+        *   Adding data in used_vehicle table  
+        */
+        if(!isset($_POST['vehicle-condition']) || $_POST['vehicle-condition'] == 2){
+            $data = [];
+            $data['token'] = $_POST['token'];
+            $data['vehicle-id'] = $vehicleId;
+            $data['vehicle-owners'] = $_POST['vehicle-owners'];
+            $data['vehicle-owner-message'] = $_POST['vehicle-owner-message'];
+            $data['vehicle-travelled-distance'] = $_POST['vehicle-travelled-distance'];
+            $data['vehicle-registered-year'] = $_POST['vehicle-registered-year'];
+            $data['vehicle-province'] = $_POST['vehicle-province'];
+
+            $cURLConnection = curl_init(API_ENDPOINT.'/used/vehicle/add');
+            curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+            $apiResponse = curl_exec($cURLConnection);
+            curl_close($cURLConnection);
+            $apiResponse = json_decode($apiResponse,TRUE);
+        }
+
+        /*
+        *   Adding data in vehicle_color_list table  
+        */
+        $data = [];
+        $data['token'] = $_POST['token'];
+        $data['vehicle-id'] = $vehicleId;
+        $data['color-id'] = json_encode($_POST['vehicle-color']);
+
+        $cURLConnection = curl_init(API_ENDPOINT.'/vehicle/color/add');
+        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+        $apiResponse = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+        $apiResponse = json_decode($apiResponse,TRUE);
+
+
+        /*
+        *   Redirect with success message  
+        */
+        setcookie('toast_message', "Vehicle added successfully", time()+60*60, "/");
+        header('Location: ../profile');
+        exit;
     }
