041e989017c186b8128e3a1d5474c7972e33d0f4
Ditxit <gau.manish77@gmail.com>
2020-12-31 20:39:44 +0545

    Started working on vehicle details displaying page

--- vehicle/index.php
+++ vehicle/index.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+    
+    if(!isset($_GET['id']) || $_GET['id'] < 1){
+        setcookie('toast_message', "Unknown vehicle", time()+60*60, "/");
+        header('Location: ../explore');
+        exit;
+    }
 
     $PAGE_NAME = "Vehicle";
 
@@ -22,35 +28,45 @@
         include_once '../include/toast.php';
     ?>
 
-    <div class="outer-container">
-        <div class="inner-container">
+    <?php
+        $vehicle = file_get_contents(API_ENDPOINT.'/vehicle/'.$_GET['id']);
+        $vehicle = json_decode($vehicle,TRUE);
+        // echo "<pre>";
+        //     var_dump($vehicle);
+        // echo "</pre>"
 
-            <div id="vehicle-info-section" class="is-white margin-y-50 shadow-20 radius-20 padding-40">
-                <div class="row">
-                    <!-- vehicle image column -->
-                    <div class="col">
-                        <div class="radius-20 is-white-95">
-                            <img src="../assets/avatars/default.jpg" alt="Vehicle Image"/>
+        echo '
+            <div class="outer-container">
+                <div class="inner-container">
+
+                    <div id="vehicle-info-section" class="is-white margin-y-50 shadow-20 radius-20 padding-40">
+                        <div class="row">
+                            <!-- vehicle image column -->
+                            <div class="col">
+                                <div class="radius-20 is-white-95">
+                                    <img src="../api/storage/'.$vehicle['images'][0]['name'].'" alt="Vehicle Image"/>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="padding-left-40">
+                                    <p class="h1">'.$vehicle['name'].'</p>
+                                    <p class="h6">
+                                        <span>NRs.</span>
+                                        <span>19 Lakh</span>
+                                    </p>
+                                    <p class="">
+                                        This is owner\'s message section
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="padding-left-40">
-                            <p class="h1">This is vehicle name</p>
-                            <p class="h6">
-                                <span>NRs.</span>
-                                <span>19 Lakh</span>
-                            </p>
-                            <p class="">
-                                This is owner's message section
-                            </p>
-                        </div>
-                    </div>
+
                 </div>
             </div>
+        ';
 
-        </div>
-    </div>
-
+    ?>
 
 
 </body>
