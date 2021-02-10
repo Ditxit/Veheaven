7d700987ab99b131a45ed84d8812449762c9f995
Ditxit <gau.manish77@gmail.com>
2021-02-10 23:30:42 +0545

    WIP: Search box UI & JS suggestion implementaion

--- include/navbar.ui.php
+++ include/navbar.ui.php
@@ -1,62 +1,147 @@
 
 <div class="outer-container is-white-100 shadow-15 sticky top">
     <div class="inner-container">
-        <div class="float-left">
-            <a href="">
-                <!--<img src="" alt="LOGO" class="padding-15"/>-->
-                <p class="h3 bold padding-15">Veheaven</p>
-            </a>
-        </div>
-        <div class="float-right">
+        <div class="row">
 
-            <?php
+            <!-- Logo Container -->
+            <div class="col-30">
+                <a href="" class="float-left">
+                    <!--<img src="" alt="LOGO" class="padding-15"/>-->
+                    <p class="h3 bold padding-15">Veheaven</p>
+                </a>
+            </div>
 
-                // Including global constants
-                include_once 'config.php';
+            <!-- Search Box Container -->
+            <div class="col-40 padding-top-15">
+                <form action="">
+                    <input class="radius-5 is-white-95 custom-border" type="search" name="search" placeholder="Search">
+                    <div id="search-suggestion" class="is-white shadow-20 radius-5" style="display:none; position:absolute; top:40;">
+                        <!-- List items will be add in JS -->
+                    </div>
+                </form>
+                <script>
+                    function textToNode(text) {
+                        var template = document.createElement('template');
+                        text = text.trim(); // Never return a text node of whitespace as the result
+                        template.innerHTML = text;
+                        return template.content.firstChild;
+                    }
 
-                // Checking user exist or not
-                if (isset($_COOKIE['token'])) {
-                    $token = file_get_contents(API_ENDPOINT.'/user-token/verify/'.$_COOKIE['token']);
-                    $token = json_decode($token,TRUE);
-                    if(isset($token) && $token['success']){
+                    const searchBox = document.getElementsByName('search')[0];
+                    const searchBoxSuggestion = document.getElementById('search-suggestion');
 
-                        $payload = file_get_contents(API_ENDPOINT.'/token/payload/'.$_COOKIE['token']);
-                        $payload = json_decode($payload,TRUE);
+                    let keyword = "";
 
-                        if(isset($payload) && $payload['success']){   
-                            $USER = [];
-                            $USER['id'] = $payload['payload']['id'];
-                            $USER['name'] = $payload['payload']['first_name'];
+                    // Event to suggest the vehicle name
+                    searchBox.oninput = function(event){
+                        keyword = searchBox.value.trim().toLowerCase();
+                        searchBoxSuggestion.innerHTML = "";
+
+                        if(keyword.length < 1){
+                            searchBoxSuggestion.style.display = 'none';
+                            return;
+                        }else{
+                            searchBoxSuggestion.style.display = 'block';
+                            for(var i=0; i < 7; i++){
+                                const data = Math.random().toString(36).substring(2);
+                                searchBoxSuggestion.appendChild(
+                                    textToNode(
+                                        "<a data-active='0' data-keyword='"+data+"' class='width-100 padding-y-10 padding-x-15 custom-border-bottom'>" +
+                                            data +
+                                        "</a>"
+                                    )
+                                );
+                            }
                         }
                     }
-                }
 
-                $CURRENT_PAGE = isset($PAGE_NAME) ? $PAGE_NAME : "";
+                    searchBox.onfocus = () => {searchBoxSuggestion.style.display = 'block';}
+                    searchBox.onblur = () => {searchBoxSuggestion.style.display = 'none';}
 
-                if($PAGE_NAME == "Explore"){
-                    echo '<a class="button padding-15 text-deep-purple">Explore</a>';
-                }else{
-                    echo '<a href="../explore" class="button padding-15" on-hover="text-deep-purple">Explore</a>';
-                }
+                    // Event for key (up-arrow,down-arrow) navigation
+                    searchBox.onkeydown = function(event){
+                        if (searchBoxSuggestion.childElementCount == 0 || ![38,40].includes(event.keyCode)) return;
 
-                if(!isset($USER)){
-                    if($PAGE_NAME == "Login"){
-                        echo '<a class="button padding-15 text-deep-purple">Login</a>';
-                    }else{
-                        echo '<a href="../login" class="button padding-15" on-hover="text-deep-purple">Login</a>';
+                        var active = searchBoxSuggestion.querySelector('a[data-active="1"]');
+
+                        if(active){
+                            active.setAttribute("data-active", "0");
+                            active.style.backgroundColor = null;
+                        }
+
+                        if(event.keyCode == 38){ // up-arrow
+                            active = active ? active.previousSibling : searchBoxSuggestion.lastChild;
+                        }else if(event.keyCode == 40){ // down-arrow
+                            active = active ? active.nextSibling : searchBoxSuggestion.firstChild;
+                        }else{}
+
+                        if(active){
+                            active.setAttribute("data-active", "1");
+                            active.style.backgroundColor = "#EEEEEE";
+
+                            searchBox.value = active.getAttribute("data-keyword");
+                        }else{
+                            searchBox.value = keyword;
+                        }
+
+                        // To fix the cursor placement at the
+                        // beginning when up arrow is pressed
+                        event.preventDefault();
                     }
-                }else{
-                    if($PAGE_NAME == "Profile"){
-                        echo '<a href="../profile" class="button padding-15 text-deep-purple">'.$USER['name'].'</a>';
-                    }else{
-                        echo '<a href="../profile" class="button padding-15" on-hover="text-deep-purple">'.$USER['name'].'</a>';
-                    }
-                }
+                </script>
+            </div>
 
-                unset($CURRENT_PAGE);
-                unset($USER);
-                
-            ?>
+            <!-- Navbar Buttons Container -->
+            <div class="col-30">
+                <div class="float-right">
+                    <?php
+                        // Including global constants
+                        include_once 'config.php';
+
+                        // Checking user exist or not
+                        if (isset($_COOKIE['token'])) {
+                            $token = file_get_contents(API_ENDPOINT.'/user-token/verify/'.$_COOKIE['token']);
+                            $token = json_decode($token,TRUE);
+                            if(isset($token) && $token['success']){
+
+                                $payload = file_get_contents(API_ENDPOINT.'/token/payload/'.$_COOKIE['token']);
+                                $payload = json_decode($payload,TRUE);
+
+                                if(isset($payload) && $payload['success']){   
+                                    $USER = [];
+                                    $USER['id'] = $payload['payload']['id'];
+                                    $USER['name'] = $payload['payload']['first_name'];
+                                }
+                            }
+                        }
+
+                        $CURRENT_PAGE = isset($PAGE_NAME) ? $PAGE_NAME : "";
+
+                        if($PAGE_NAME == "Explore"){
+                            echo '<a class="button padding-15 text-deep-purple">Explore</a>';
+                        }else{
+                            echo '<a href="../explore" class="button padding-15" on-hover="text-deep-purple">Explore</a>';
+                        }
+
+                        if(!isset($USER)){
+                            if($PAGE_NAME == "Login"){
+                                echo '<a class="button padding-15 text-deep-purple">Login</a>';
+                            }else{
+                                echo '<a href="../login" class="button padding-15" on-hover="text-deep-purple">Login</a>';
+                            }
+                        }else{
+                            if($PAGE_NAME == "Profile"){
+                                echo '<a href="../profile" class="button padding-15 text-deep-purple">'.$USER['name'].'</a>';
+                            }else{
+                                echo '<a href="../profile" class="button padding-15" on-hover="text-deep-purple">'.$USER['name'].'</a>';
+                            }
+                        }
+
+                        unset($CURRENT_PAGE);
+                        unset($USER);       
+                    ?>
+                </div>
+            </div>
 
         </div>
     </div>
