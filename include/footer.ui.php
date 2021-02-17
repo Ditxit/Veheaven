d4d95307745d4e926e367eeebccc17f5edb8ce99
Ditxit <gau.manish77@gmail.com>
2021-02-17 23:09:22 +0545

    Create a footer ui file in include directory

---
+++ include/footer.ui.php
@@ -0,0 +1,26 @@
+<div class="outer-container is-white margin-top-30 custom-border-top">
+    <div class="width-80 float-center padding-y-40">
+        <div class="row">
+            <div class="col">
+                <p class="h6 margin-y-10 custom-text-red">Overview</p>
+                <p><a href="" class="small margin-y-5 custom-text-blue">About Us</a></p>
+                <p><a href="" class="small margin-y-5 custom-text-blue">FAQs</a></p>
+                <p><a href="" class="small margin-y-5 custom-text-blue">Privacy Policies</a></p>
+                <p><a href="" class="small margin-y-5 custom-text-blue">Terms & Conditions</a></p>
+            </div>
+            <div class="col">
+                <p class="h6 margin-y-10 custom-text-red">Contact Us</p>
+                <p><a href="Mailto:gau.manish77@gmail.com" class="small margin-y-5 custom-text-blue">Write us an Email</a></p>
+                <p><a href="tel:+977-9863689659" class="small margin-y-5 custom-text-blue">Make a Phone Call</a></p>
+            </div>
+            <div class="col">
+                <iframe class="radius-10 custom-bg-gray" width="480" height="158" src="https://maps.google.com/maps?q=Islington%20college&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="outer-container is-white custom-border-top">
+    <div class="width-80 float-center padding-y-30">
+        <p class="h6">&copy; Manish Gautam</p>
+    </div>
+</div>
